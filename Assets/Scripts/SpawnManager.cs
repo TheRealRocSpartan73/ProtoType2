094f8a5de447411c54e6cd7f2ced8132844b2974
TheRealRocSpartan73 <146456843+TheRealRocSpartan73@users.noreply.github.com>
2023-10-08 00:03:13 +0100

    Lesson 2.4.3 complete

--- Assets/Scripts/SpawnManager.cs
+++ Assets/Scripts/SpawnManager.cs
@@ -8,23 +8,27 @@ public class SpawnManager : MonoBehaviour
     public GameObject[] animalPrefabs;  //Collection of animals to spawn
     private float spawnRangeX = 20;
     private float spawnRangeZ = 20;
+    private float startDelay = 2f;
+    private float spawnInterval = 1.5f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", spawnInterval, 1.5f);  
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            int animalIndex = 0;  //Index into animalPrefabs array
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX),0, spawnRangeZ); //Create a new random spawn vector
-            
-            animalIndex = Random.Range(0, animalPrefabs.Length); //Randomly select our animal object
 
-            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
-        }
     }
+
+    void SpawnRandomAnimal()
+    {
+        int animalIndex = Random.Range(0, animalPrefabs.Length);  ////Randomly select our animal object index
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnRangeZ); //Create a new random spawn vector
+
+        //Create and spawn the animal object into the game scene
+        Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+    }
+
 }
